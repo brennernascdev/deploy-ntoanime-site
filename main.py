6f39650dbc17db57077ff7e89a5694d5c02dd90e
brennernascdev <jbrenner.dev@gmail.com>
2025-07-21 01:38:33 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 serverIp = '191.252.223.223'
 serverPort = 7171
 download_links = {
-    'old': "https://drive.google.com/file/d/1jzg1EHhpyY_d2XymUnVpC2WsN_ipjT1G/view?usp=sharing",
+    'oldc': "https://drive.google.com/file/d/1jzg1EHhpyY_d2XymUnVpC2WsN_ipjT1G/view?usp=sharing",
     'otc': "https://drive.google.com/file/d/16KBcWn46ov12N67lHKZKiTOpvr9csxN1/view?usp=sharing"
 }
 
@@ -28,4 +28,4 @@ def homepage():
     return render_template("index.html", online=serverOnline, links=download_links)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
