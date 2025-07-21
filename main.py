13c2843278b682e40bf06ef5d0bcbd2defd51f07
brennernascdev <jbrenner.dev@gmail.com>
2025-07-21 00:50:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 serverIp = '191.252.223.223'
 serverPort = 7171
-download_link = "https://drive.google.com/file/d/1oVkdskoQzwdHk6p4cGU2rkCadf_Ms6oy/view?usp=drive_link"
+download_links = {
+    'old': "https://drive.google.com/file/d/1jzg1EHhpyY_d2XymUnVpC2WsN_ipjT1G/view?usp=sharing",
+    'otc': "https://drive.google.com/file/d/16KBcWn46ov12N67lHKZKiTOpvr9csxN1/view?usp=sharing"
+}
 
 def getServerState():
     global serverIp, serverPort
@@ -22,7 +25,7 @@ def homepage():
     global serverOnline
     serverOnline = getServerState()
     print(serverOnline)
-    return render_template("index.html", online=serverOnline)
+    return render_template("index.html", online=serverOnline, links=download_links)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
